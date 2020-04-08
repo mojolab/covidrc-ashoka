4e30e856035c1c7b49f4ed4a6771580b9f4a9d03
Arjun Venkatraman <mojoarjun@gmail.com>
2020-04-08 13:19:56 +0530

    Replicating for COVID-19 Tracker

---
+++ client/src/components/SecNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import info from '../icons/info.svg'
+import twitter from '../icons/twitter.svg'
+import {PageView, Event} from "./Tracking"
+
+export function SecNav(props) {
+    let {handleAboutClicked, desktopSize} = props
+    return (
+        <ul className="secNavContainer">
+            <li><a
+                href="https://hackergram.org"
+                datacontent="COVID-19 Resource Center"
+                style={{backgroundImage: `url(${info})`}}
+                onClick={()=> {
+                    handleAboutClicked();
+                    PageView('About');
+            }}></a></li>
+            {}
+        </ul>
+    )
+}
